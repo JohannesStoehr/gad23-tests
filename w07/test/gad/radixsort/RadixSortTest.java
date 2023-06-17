e1c8c74d5d6913868864f5327ff315496918d2fb
Finn Hartmann <114213894+Flnn0@users.noreply.github.com>
2023-06-17 18:04:10 +0200

    [W07] Adding last year tests and tests in RadixSortTest and BinaryRadixSortTest (#35)
    <!-- Thanks for contributing to Artemis! Before you submit your pull
    request, please make sure to check the following boxes by putting an x
    in the [ ] (don't: [x ], [ x], do: [x]) -->
    <!-- If your pull request is not ready for review yet, create a draft
    pull request! -->
    
    ### Checklist
    #### General
    <!-- You only need to choose one of the first two check items:
    Generally, test on the test servers. -->
    <!-- If it's only a small change, testing it locally is acceptable and
    you may remove the first checkmark. If you are unsure, please test on
    the test servers. -->
    - [x] I confirm that my tests, if any, are correct
        - [x] Optional: I pass **all** artemis tests.
        - [x] Optional: I pass **all** tests, that are added in this PR.
    - [x] Optional: I calculated manually the correct solutions on paper.
    - [x] I chose a title conforming to the [naming conventions for pull
    requests](https://github.com/JohannesStoehr/gad23-tests/blob/main/CONTRIBUTING.md).
    - [x] The newly added tests do not reveal the solutions dynamically
    calculating the solutions as expected in the exercise.
    - [x] Other students approved this Pull Request.
    
    ### Description
    <!-- Describe your changes shortly -->
    - `RadixSortTest`: I added two Edge-Casas for `provideKey` and in
    `provideKeyInvalidIndex` invalid Indices
    - `BinaryRadixSortTest`: I added `sortOnlyNegativTest` which test if
    negativ numbers are sorted correcly only works if `lastSort` is
    implemented, `sortOnlyPositivTest` which test if positiv numbers are
    sorted correcly, `sortUsualTest` contains positiv and negativ Numbers,
    also `lastSort` must be implemented, that this test works and
    `sortBigTest` is the same as `sortUsualTest` but there are more numbers
    which should be sorted. This test could fail on some devices but is also
    repeated in the code.

---
+++ w07/test/gad/radixsort/RadixSortTest.java
@@ -0,0 +1,63 @@
+package gad.radixsort;
+
+import gad.radix.RadixSort;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.Arguments;
+import org.junit.jupiter.params.provider.MethodSource;
+
+import java.util.stream.Stream;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class RadixSortTest {
+
+    @ParameterizedTest
+    @MethodSource("provideMaxDecimalPlaces")
+    void testMaxDecimalPlaces(int[] elements, int expectedMax) {
+        assertEquals(expectedMax, RadixSort.getMaxDecimalPlaces(elements));
+    }
+
+    private static Stream<Arguments> provideMaxDecimalPlaces() {
+        return Stream.of(
+                Arguments.of(new int[]{1, 2, 5675, 222222, 930}, 6),
+                Arguments.of(new int[]{1, 2, 5675990, -222222, 930}, 7),
+                Arguments.of(new int[]{0, 0, 0, 0}, 1),
+                Arguments.of(new int[]{0, -1, 0, 0}, 1),
+                Arguments.of(new int[]{0, -889, 0, 0}, 3),
+                Arguments.of(new int[]{Integer.MAX_VALUE, Integer.MIN_VALUE, 0}, 10),
+                Arguments.of(new int[]{1, 2, 5675, -222222, 930, -1, 999999, 1000000}, 7)
+        );
+    }
+
+    @ParameterizedTest
+    @MethodSource("provideKey")
+    void testKey(int element, int decimalPlace, int expected) {
+        assertEquals(expected, RadixSort.key(element, decimalPlace));
+    }
+
+    private static Stream<Arguments> provideKey() {
+        return Stream.of(
+                Arguments.of(1234, 3, 1),
+                Arguments.of(981739, 2, 7),
+                Arguments.of(439183, 0, 3),
+                Arguments.of(0, 99, 0),
+                //Edge-Cases
+                Arguments.of(6, 0, 6),
+                Arguments.of(0, 0, 0)
+        );
+    }
+
+    @ParameterizedTest
+    @MethodSource("provideKeyInvalidIndex")
+    void testKeyWithInvalidIndex(int element, int decimalPlace, int expected) {
+        assertEquals(expected, RadixSort.key(element, decimalPlace));
+    }
+
+    private static Stream<Arguments> provideKeyInvalidIndex() {
+        return Stream.of(
+                Arguments.of(12345, 5, 0),
+                Arguments.of(987654321, 10, 0),
+                Arguments.of(0, 0, 0)
+        );
+    }
+}
